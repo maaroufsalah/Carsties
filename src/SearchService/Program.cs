3473a5bd5a52fc354488b8dacf91ef7beb25dbf5
Salah-Eddine <devmaarouf@outlook.com>
2024-11-06 12:22:41 +0100

    Adding RabbitMQ

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,6 +1,9 @@
 using System.Net;
+using Contracts;
+using MassTransit;
 using Polly;
 using Polly.Extensions.Http;
+using SearchService.Consumers;
 using SearchService.Data;
 using SearchService.Services;
 
@@ -9,9 +12,24 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+builder.Services.AddMassTransit(x => 
+{
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
 
-builder.Services.AddEndpointsApiExplorer();
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ReceiveEndpoint("search-auction-created", e =>
+        {
+            e.UseMessageRetry(r => r.Interval(5, 5));
+            e.ConfigureConsumer<AuctionCreatedConsumer>(context);
+        });
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 var app = builder.Build();
 
