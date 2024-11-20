ee571356d63cbdf0a0a5eb76e634268158e25587
Salah-Eddine <devmaarouf@outlook.com>
2024-11-20 10:59:48 +0100

    End of implementing  IdentityServer in frontend with AuthJS

---
+++ frontend/web-app/app/api/auth/signin/page.tsx
@@ -0,0 +1,15 @@
+import EmptyFilter from '@/app/components/EmptyFilter'
+import React from 'react'
+
+export default function SignIn({searchParams}: {searchParams: {callbackUrl: string}}) {
+  return (
+    <div>
+        <EmptyFilter 
+            title='You need to be logged in to do that'
+            subtitle='Please click below to login'
+            showLogin
+            callbackUrl={searchParams.callbackUrl}
+        />
+    </div>
+  )
+}
